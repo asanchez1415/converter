ecc64fd6af320c12b0ecf59101d8253d161a9b3a
asanchez1415 <76190586+asanchez1415@users.noreply.github.com>
2021-06-26 22:06:56 -0400

    Se terminaron las unidades de masa, volumen y distancia, por revisar

---
+++ js/calc/distanceCalculate.js
@@ -0,0 +1,183 @@
+function distanceCalculate() {
+  let numberQuantity = parseFloat(
+    document.getElementById("numberQuantity").value
+  );
+  let optionDistance = document.getElementById("optionDistance").value;
+
+  switch (optionDistance) {
+    /* Caso para calcular desde los milimetros */
+    case "mm":
+      cm = numberQuantity / 10;
+      m = cm / 100;
+      km = m / 1000;
+      //Ya tiene los mm
+      yarda = numberQuantity / 914;
+      pulgadas = yarda * 36;
+
+      unitAndResult = [
+        { unit: "cm", result: cm },
+        { unit: "in", result: pulgadas },
+        { unit: "yd", result: yarda },
+        { unit: "m", result: m },
+        { unit: "Km", result: km },
+      ];
+      optionR = optionDistance;
+      break;
+
+    /* Caso para calcular desde los centimetros */
+    case "cm":
+      m = numberQuantity / 100;
+      km = m / 1000;
+      //Ya tiene los cm
+      yarda = numberQuantity / 91,44;
+      pulgadas = yarda * 36;
+	    mm = numberQuantity * 10;
+
+      unitAndResult = [
+        { unit: "mm", result: mm },
+        { unit: "in", result: pulgadas },
+        { unit: "yd", result: yarda },   
+        { unit: "m", result: m },
+        { unit: "Km", result: km },
+      ];
+      optionR = optionDistance;
+      break;
+
+    /* Caso para calcular desde las pulgadas */
+    case "in":
+      yarda = numberQuantity / 36;
+      //Ya tiene la Pulgada
+      mm = numberQuantity * 25,4;
+      cm = mm / 10;
+      m = cm / 100;
+      km = m / 1000;
+
+      unitAndResult = [
+        { unit: "mm", result: mm },
+        { unit: "cm", result: cm },
+        { unit: "yd", result: yarda },
+        { unit: "m", result: m },
+        { unit: "Km", result: km },
+      ];
+      optionR = optionDistance;
+      break;
+
+    /* Caso para calcular desde las yardas */
+    case "yd":
+      //Ya tiene la Yarda
+      pulgadas = numberQuantity * 36;
+      mm = numberQuantity * 914,4;
+      cm = mm / 10;
+      m = cm / 100;
+      Km = m / 1000;
+
+      unitAndResult = [
+        { unit: "mm", result: mm },
+        { unit: "cm", result: cm },
+        { unit: "in", result: pulgadas },
+        { unit: "m", result: m },
+        { unit: "km", result: km },
+      ];
+      optionR = optionDistance;
+      break;
+
+    /* Caso para calcular desde los metros */
+    case "m":
+      km = numberQuantity / 1000;
+      //Ya tiene los m
+      yarda = numberQuantity * 1,094;
+      pulgadas = yarda * 36;
+      cm = numberQuantity * 100;
+	    mm = cm * 10;
+
+      unitAndResult = [
+        { unit: "mm", result: mm },
+        { unit: "cm", result: cm },
+        { unit: "in", result: pulgadas },
+        { unit: "yd", result: yarda },
+        { unit: "km", result: km },
+      ];
+      optionR = optionDistance;
+      break;
+
+    /* Caso para calcular desde los kilometros */
+    case "Km":
+      //Ya tiene los Km
+      m = numberQuantity * 1000;
+      yarda = m * 1,094;
+      pulgadas = yarda * 36;
+      cm = m * 100;
+	    mm = cm * 10;
+
+      unitAndResult = [
+        { unit: "mm", result: mm },
+        { unit: "cm", result: cm },
+        { unit: "in", result: pulgadas },
+        { unit: "yd", result: yarda },
+        { unit: "m", result: m },
+      ];
+      optionR = optionDistance;
+      break;
+  }
+  showResult(); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* function timeCalculate() {
+    let numberQuantity = parseInt(
+      document.getElementById("numberQuantity").value
+    );
+    let optionTime = document.getElementById("optionTime").value;
+  
+    switch (optionTime) {
+      case "s":
+        //Ya tiene los s
+        min = numberQuantity / 60;
+        h = min / 60;
+  
+        unitResult = JSON.stringify({
+          min: min,
+          h: h,
+        });
+        optionTimeR = optionTime;
+        break;
+  
+      case "min":
+        s = numberQuantity * 60;
+        //Ya tiene los min
+        h = numberQuantity / 60;
+  
+        unitResult = JSON.stringify({
+          s: s,
+          h: h,
+        });
+        optionTimeR = optionTime;
+        break;
+  
+      case "h":
+        min = numberQuantity * 60;
+        s = min * 60;
+        //Ya tiene los s
+  
+        unitResult = JSON.stringify({
+          s: s,
+          min: min,
+        });
+        optionTimeR = optionTime;
+        break;
+    }
+  } */
---
+++ resources/lateral-menu.js
@@ -0,0 +1,48 @@
+document.getElementById("lateral-menu").innerHTML = `<ul id="slide-out" class="sidenav lateral-menu">
+<li>
+  <div class="user-view">
+    <div class="background">
+      <img src="../images/galaxy.jpg">
+    </div>
+    <h5 class="white-text">CONVERSOR</h5>
+  </div>
+</li>
+<li><a href="#" class="white-text">Opciones</a></li>
+<li>
+  <div class="divider"></div>
+</li>
+<li>
+  <a class="waves-effect white-text" href="mass.html"><i class="fas
+  fa-balance-scale-right white-text"></i>MASA</a>
+</li>
+
+<li>
+  <a class="waves-effect white-text" href="volume.html"><i class="fas 
+  fa-cube white-text"></i>VOLUMEN</a>
+</li>
+
+<li>
+  <a class="waves-effect white-text" href="distance.html"><i class="fas 
+  fa-ruler white-text"></i>DISTANCIA</a>
+</li>
+
+<li>
+  <a class="waves-effect white-text" href="mass.html"><i class="fas fa-tint white-text"></i>DENSIDAD</a>
+</li>
+
+<li>
+  <a class="waves-effect white-text" href="mass.html"><i class="fas fa-clock white-text"></i>TIEMPO</a>
+</li>
+
+<li>
+  <a class="waves-effect white-text" href="mass.html"><i class="fas fa-people-carry white-text"></i>FUERZA</a>
+</li>
+
+<li>
+  <a class="waves-effect white-text" href="mass.html"><i class="fas fa-battery-half white-text"></i>ENERGÍA</a>
+</li>
+
+<li>
+  <a class="waves-effect white-text" href="mass.html"><i class="far fa-futbol white-text"></i></i>PRESIÓN</a>
+</li>
+</ul>`
